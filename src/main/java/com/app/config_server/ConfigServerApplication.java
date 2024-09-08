84564ca5e6b87cc1457e8461df0b2789ae053387
Sharad <sharadchavhan001@gmail.com>
2024-09-08 18:28:28 +0530

    Update ConfigServerApplication.java

--- src/main/java/com/app/config_server/ConfigServerApplication.java
+++ src/main/java/com/app/config_server/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.app.config_server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,9 +9,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-		System.out.println("ConfigServerApplication started successfully");
+		logger.info("Starting the Config Server application...");
+        SpringApplication.run(ConfigServerApplication.class, args);
+        logger.info("Config Server application started successfully.");
 	}
 }
 
